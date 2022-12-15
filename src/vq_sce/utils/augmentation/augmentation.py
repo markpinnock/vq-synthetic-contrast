e8a5881f6e929fb73abf7186acb233852842019e
Mark <rmappin@ucl.ac.uk>
2022-12-15 16:42:17 +0000

    Augmentation updated for new image dims

--- src/vq_sce/utils/augmentation/augmentation.py
+++ src/vq_sce/utils/augmentation/augmentation.py
@@ -1,6 +1,9 @@
+import numpy as np
 import tensorflow as tf
 
 from vq_sce.utils.augmentation.affine_transform import AffineTransform2D
+
+DEG_TO_RAD = 180 * np.pi
 
 
 #-------------------------------------------------------------------------
@@ -8,32 +11,46 @@ from vq_sce.utils.augmentation.affine_transform import AffineTransform2D
 
 class StdAug(tf.keras.layers.Layer):
 
-    def __init__(self, config, name="std_aug"):
+    def __init__(self, config, name="std_aug") -> None:
         super().__init__(name=name)
 
-        # If segmentations available, these can be stacked on the target for transforming
-        if "segs" in config.keys():
-            self.transform = AffineTransform2D(config["img_dims"] + [2])
-        else:
-            self.transform = AffineTransform2D(config["img_dims"] + [1])
+        self.source_affine_transform = AffineTransform2D(config["source_dims"])
+        self.target_affine_transform = AffineTransform2D(config["target_dims"])
+        self._matrix_indices = None
 
-        self.flip_probs = tf.math.log([[config["flip_prob"], 1 - config["flip_prob"]]])
-        self.rot_angle = config["rotation"] / 180 * 3.14159265359
-        self.scale_factor = config["scale"]
-        self.shear_angle = config["shear"] / 180 * 3.14159265359
-        self.x_shift = [-config["translate"][0], config["translate"][0]]
-        self.y_shift = [-config["translate"][1], config["translate"][1]]
+        self._flip_probs = tf.math.log([[config["flip_prob"], 1 - config["flip_prob"]]])
+        self._rot_angle = config["rotation"] / DEG_TO_RAD
+        self._scale_factor = config["scale"]
+        self._shear_angle = config["shear"] / DEG_TO_RAD
+        self._x_shift = [-config["translate"][0], config["translate"][0]]
+        self._y_shift = [-config["translate"][1], config["translate"][1]]
 
-    def flip_matrix(self, mb_size: int):
-        updates = tf.reshape(tf.cast(tf.random.categorical(logits=self.flip_probs, num_samples=mb_size * 2), "float32"), [mb_size * 2])
+    def _create_matrix_indices(self, mb_size: int) -> None:
+        """ Create indices to update transformation matrix elements """
+    
+        mb_indices = tf.repeat(tf.range(0, mb_size), 2)[:, tf.newaxis]
+        element_indices = tf.tile(tf.constant([[0, 0], [1, 1]]), [mb_size, 1])
+        self._matrix_indices = tf.concat([mb_indices, element_indices], axis=1)
+
+    def _flip_matrix(self, mb_size: int) -> tf.Tensor:
+        """ Create flip matrix """
+
+        updates = tf.random.categorical(
+            logits=self._flip_probs,
+            num_samples=mb_size * 2
+        )
+        updates = tf.squeeze(tf.cast(updates, "float32"))
         updates = 2.0 * updates - 1.0
-        indices = tf.concat([tf.repeat(tf.range(0, mb_size), 2)[:, tf.newaxis], tf.tile(tf.constant([[0, 0], [1, 1]]), [mb_size, 1])], axis=1)
-        flip_mat = tf.scatter_nd(indices, updates, [mb_size, 2, 2])
+
+        # Create flip matrix
+        flip_mat = tf.scatter_nd(self._matrix_indices, updates, [mb_size, 2, 2])
 
         return flip_mat
 
-    def rotation_matrix(self, mb_size: int):
-        thetas = tf.random.uniform([mb_size], -self.rot_angle, self.rot_angle)
+    def _rotation_matrix(self, mb_size: int) -> tf.Tensor:
+        """ Create rotation matrix """
+
+        thetas = tf.random.uniform([mb_size], -self._rot_angle, self._rot_angle)
         rot_mat = tf.stack(
             [
                 [tf.math.cos(thetas), -tf.math.sin(thetas)],
@@ -45,61 +62,89 @@ class StdAug(tf.keras.layers.Layer):
 
         return rot_mat
 
-    def scale_matrix(self, mb_size: int):
-        updates = tf.repeat(tf.random.uniform([mb_size], * self.scale_factor), 2)
-        indices = tf.concat([tf.repeat(tf.range(0, mb_size), 2)[:, tf.newaxis], tf.tile(tf.constant([[0, 0], [1, 1]]), [mb_size, 1])], axis=1)
-        scale_mat = tf.scatter_nd(indices, updates, [mb_size, 2, 2])
+    def _scale_matrix(self, mb_size: int) -> tf.Tensor:
+        """ Create scaling matrix """
+
+        updates = tf.repeat(tf.random.uniform([mb_size], * self._scale_factor), 2)
+        scale_mat = tf.scatter_nd(self._matrix_indices, updates, [mb_size, 2, 2])
 
         return scale_mat
 
-    def shear_matrix(self, mb_size: int):
-        mask = tf.cast(tf.random.categorical(logits=[[0.5, 0.5]], num_samples=mb_size), "float32")
-        mask = tf.reshape(tf.transpose(tf.concat([mask, 1 - mask], axis=0), [1, 0]), [1, -1])
-        updates = tf.repeat(tf.random.uniform([mb_size], -self.shear_angle, self.shear_angle), 2)
+    def _shear_matrix(self, mb_size: int) -> tf.Tensor:
+        """ Create shear matrix """
+
+        mask = tf.random.categorical(logits=[[0.0, 0.0]], num_samples=mb_size)
+        mask = tf.cast(mask, "float32")
+        mask = tf.transpose(tf.concat([mask, 1 - mask], axis=0), [1, 0])
+        mask = tf.reshape(mask, [1, -1])
+
+        updates = tf.random.uniform([mb_size], -self._shear_angle, self._shear_angle)
+        updates = tf.repeat(updates, 2)
         updates = tf.reshape(updates * mask, [-1])
-        indices = tf.concat([tf.repeat(tf.range(0, mb_size), 2)[:, tf.newaxis], tf.tile(tf.constant([[0, 1], [1, 0]]), [mb_size, 1])], axis=1)
+
+        # Can't use self._matrix_indices here, as not diagonal elements
+        mb_indices = tf.repeat(tf.range(0, mb_size), 2)[:, tf.newaxis]
+        element_indices = tf.tile(tf.constant([[0, 1], [1, 0]]), [mb_size, 1])
+        indices = tf.concat([mb_indices, element_indices], axis=1)
+
         shear_mat = tf.scatter_nd(indices, updates, [mb_size, 2, 2])
         shear_mat += tf.tile(tf.eye(2)[tf.newaxis, :, :], [mb_size, 1, 1])
 
         return shear_mat
 
-    def translation_matrix(self, m, mb_size: int):
-        xs = tf.random.uniform([mb_size], *self.x_shift)
-        ys = tf.random.uniform([mb_size], *self.y_shift)
+    def _translation_matrix(self, matrix, mb_size: int) -> tf.Tensor:
+        """ Create translation matrix """
+
+        xs = tf.random.uniform([mb_size], *self._x_shift)
+        ys = tf.random.uniform([mb_size], *self._y_shift)
+
         xys = tf.stack([xs, ys], axis=0)
         xys = tf.transpose(xys, [1, 0])[:, :, tf.newaxis]
-        m = tf.concat([m, xys], axis=2)
+        matrix = tf.concat([matrix, xys], axis=2)
 
-        return m
+        return matrix
 
-    def transformation(self, mb_size: int):
+    def _transformation(self, mb_size: int) -> tf.Tensor:
         trans_mat = tf.matmul(
-            self.shear_matrix(mb_size), tf.matmul(
-                self.flip_matrix(mb_size), tf.matmul(
-                    self.rotation_matrix(mb_size), self.scale_matrix(mb_size))))
-
-        trans_mat = self.translation_matrix(trans_mat, mb_size)
+            self._rotation_matrix(mb_size),
+            self._scale_matrix(mb_size)
+        )
+        trans_mat = tf.matmul(
+            self._flip_matrix(mb_size),
+            trans_mat
+        )
+        trans_mat = tf.matmul(
+            self._shear_matrix(mb_size),
+            trans_mat
+        )
+        trans_mat = self._translation_matrix(trans_mat, mb_size)
 
         return trans_mat
     
-    def call(self, imgs, seg=None):
-        l = len(imgs)
-        imgs = tf.concat(imgs, axis=4)
-        mb_size = imgs.shape[0]
-        thetas = tf.reshape(self.transformation(mb_size), [mb_size, -1])
+    def call(
+        self,
+        source: list[tf.Tensor | None],
+        target: list[tf.Tensor | None],
+    ) -> tuple[list[tf.Tensor, None]]:
 
-        if seg is not None:
-            img_seg = tf.concat([imgs, seg], axis=4)
-            img_seg = self.transform(im=img_seg, mb_size=mb_size, thetas=thetas)
-            imgs = [img_seg[:, :, :, :, i][:, :, :, :, tf.newaxis] for i in range(l)]
-            seg = img_seg[:, :, :, :, -1][:, :, :, :, tf.newaxis]
+        aug_source, aug_target = [], []
+        mb_size = source[0].shape[0]
+        self._create_matrix_indices(mb_size)
+        thetas = tf.reshape(self._transformation(mb_size), [mb_size, -1])
 
-            return tuple(imgs), seg
-        
-        else:
-            imgs = self.transform(im=imgs, mb_size=mb_size, thetas=thetas)
-            imgs = [imgs[:, :, :, :, i][:, :, :, :, tf.newaxis] for i in range(l)]
-            return tuple(imgs)
+        for s in source:
+            if s is None:
+                aug_source.append(None)
+            else:
+                aug_source.append(self.source_affine_transform(s, thetas))
+
+        for t in target:
+            if t is None:
+                aug_target.append(None)
+            else:
+                aug_target.append(self.target_affine_transform(t, thetas))
+
+        return aug_source, aug_target
 
 
 #-------------------------------------------------------------------------
@@ -138,19 +183,19 @@ if __name__ == "__main__":
     )
 
     aug_config = test_config["augmentation"]
+    aug_config["source_dims"] = [64, 512, 512]
+    aug_config["target_dims"] = [64, 512, 512]
     TestAug = StdAug(aug_config)
 
     for data in contrast_ds.batch(2).take(2):
         pred = np.zeros_like(data["source"].numpy())
-        pred[:, 0:pred.shape[1] // 2, 0:pred.shape[1] // 2, :, :] = 1
-        pred[:, pred.shape[1] // 2:, pred.shape[1] // 2:, :, :] = 1
+        pred[:, :, 0:pred.shape[2] // 2, 0:pred.shape[3] // 2, :] = 1
+        pred[:, :, -pred.shape[2] // 2:, -pred.shape[3] // 2:, :] = 1
         inv_pred = 1 - pred
 
-        source, target, pred, inv_pred = TestAug(
-            [data["source"],
-            data["target"],
-            pred,
-            inv_pred]
+        (source, pred), (target, inv_pred) = TestAug(
+            [data["source"], pred],
+            [data["target"], inv_pred]
         )
 
         plt.subplot(2, 6, 1)
@@ -190,17 +235,19 @@ if __name__ == "__main__":
 
         plt.show()
 
+    aug_config["source_dims"] = [3, 512, 512]
+    aug_config["target_dims"] = [12, 512, 512]
+    TestAug = StdAug(aug_config)
+
     for data in super_res_ds.batch(2).take(2):
-        pred = np.zeros_like(data["source"].numpy())
-        pred[:, 0:pred.shape[1] // 2, 0:pred.shape[1] // 2, :, :] = 1
-        pred[:, pred.shape[1] // 2:, pred.shape[1] // 2:, :, :] = 1
+        pred = np.zeros_like(data["target"].numpy())
+        pred[:, :, 0:pred.shape[2] // 2, 0:pred.shape[3] // 2, :] = 1
+        pred[:, :, -pred.shape[2] // 2:, -pred.shape[3] // 2:, :] = 1
         inv_pred = 1 - pred
 
-        source, target, pred, inv_pred = TestAug(
-            [data["source"],
-            data["target"],
-            pred,
-            inv_pred]
+        (source,), (target, pred, inv_pred) = TestAug(
+            [data["source"]],
+            [data["target"], pred, inv_pred]
         )
 
         plt.subplot(2, 6, 1)
