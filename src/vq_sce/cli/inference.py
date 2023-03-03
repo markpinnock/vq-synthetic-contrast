9a1102d364a4a24500c15f0c172a7dbe5084087f
Mark <rmappin@ucl.ac.uk>
2023-03-03 17:30:24 +0000

    Add option for full size inference - to be added

--- src/vq_sce/cli/inference.py
+++ src/vq_sce/cli/inference.py
@@ -16,7 +16,64 @@ STRIDE_LENGTH = 16
 
 #-------------------------------------------------------------------------
 
-def inference(config: dict, save: bool):
+def full_size_inference(config: dict, save: bool):
+    save_path = config["paths"]["expt_path"] / "predictions"
+    save_path.mkdir(parents=True, exist_ok=True)
+    patch_size = config["data"]["patch_size"]
+    strides = [STRIDE_LENGTH, STRIDE_LENGTH, STRIDE_LENGTH]
+    mb_size = config["expt"]["mb_size"]
+
+    test_ds, TestGenerator = get_test_dataloader(
+        config=config,
+        by_subject=False,
+        stride_length=STRIDE_LENGTH
+    )
+
+    model = build_model(config=config, purpose="inference")
+    combine = CombinePatches(STRIDE_LENGTH)
+
+    for data in test_ds:
+        source = data["source"][0, ...]
+        source = tf.transpose(source, [2, 0, 1])                   # TODO THIS NEEDS CHANGING
+        subject_id = data["subject_id"][0].numpy().decode("utf-8")
+        combine.new_subject(source.shape)
+
+        linear_coords = generate_indices(source.shape, strides, patch_size)
+        patch_stack = extract_patches(source, linear_coords, patch_size)
+        patch_stack = tf.transpose(patch_stack, [0, 2, 3, 1, 4])    # TODO CHANGE
+        num_patches = patch_stack.shape[0]
+        pred_stack = []
+
+        for i in range(0, num_patches, mb_size):
+            pred_mb, _ = model(patch_stack[i:(i + mb_size), ...])
+            pred_mb = TestGenerator.un_normalise(pred_mb)
+            pred_stack.extend(pred_mb)
+
+        pred_stack = tf.stack(pred_stack, axis=0)[:, :, :, :, 0]
+        pred_stack = tf.transpose(pred_stack, [0, 3, 1, 2])    # TODO CHANGE
+        combine.apply_patches(pred_stack, tf.stack(linear_coords, axis=0))
+        pred = combine.get_img()
+        pred = tf.transpose(pred, [1, 2, 0]).numpy()    # TODO CHANGE
+
+        if save:
+            img_nrrd = itk.GetImageFromArray(pred.astype("int16").transpose([2, 0, 1]))
+            itk.WriteImage(img_nrrd, str(save_path / f"{subject_id}.nrrd"))
+            print(f"{subject_id} saved")
+
+        else:
+            print(subject_id)
+            plt.subplot(1, 2, 1)
+            plt.imshow(pred[:, :, 32], cmap="gray", vmin=-150, vmax=250)
+            plt.axis("off")
+            plt.subplot(1, 2, 2)
+            plt.imshow(np.flipud(pred[128, :, :].T), cmap="gray", vmin=-150, vmax=250)
+            plt.axis("off")
+            plt.show()
+
+
+#-------------------------------------------------------------------------
+
+def patch_inference(config: dict, save: bool):
     save_path = config["paths"]["expt_path"] / "predictions"
     save_path.mkdir(parents=True, exist_ok=True)
     patch_size = config["data"]["patch_size"]
@@ -179,10 +236,14 @@ def main():
     config["data"]["data_path"] = Path(arguments.data)
     config["expt"]["mb_size"] = arguments.minibatch
 
-    if len(config["hyperparameters"]["scales"]) == 1:
-        inference(config, arguments.save)
-    else:
+    if config["hyperparameters"]["scales"] == [1]:
+        full_size_inference(config, arguments.save)
+    elif config["hyperparameters"]["scales"] == [4]:
+        patch_inference(config, arguments.save)
+    elif len(config["hyperparameters"]["scales"]) > 1:
         multiscale_inference(config, arguments.save)
+    else:
+        raise ValueError(f"Scales not recognised: {config['hyperparameters']['scales']}")
 
 
 #-------------------------------------------------------------------------
