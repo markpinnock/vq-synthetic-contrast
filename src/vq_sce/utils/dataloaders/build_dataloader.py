a777c6e1e339c5ce8d6c90db33c24649b6daa81f
Mark Pinnock <rmappin@ucl.ac.uk>
2023-03-24 13:20:33 +0000

    Modify get_test_dataloader to return other subsets

--- src/vq_sce/utils/dataloaders/build_dataloader.py
+++ src/vq_sce/utils/dataloaders/build_dataloader.py
@@ -1,3 +1,4 @@
+import enum
 import glob
 from pathlib import Path
 import tensorflow as tf
@@ -20,6 +21,19 @@ DataloaderType = tuple[
 INFERENCE_MB_SIZE = 1
 
 
+#-------------------------------------------------------------------------
+
+
+@enum.unique
+class Subsets(str, enum.Enum):
+    TRAIN = "training"
+    VALID = "validation"
+    TEST = "test"
+
+
+#-------------------------------------------------------------------------
+
+
 def get_train_dataloader(config: dict[str, Any], dev: bool) -> DataloaderType:
 
     # Specify output types and scale
@@ -27,8 +41,8 @@ def get_train_dataloader(config: dict[str, Any], dev: bool) -> DataloaderType:
 
     # Initialise datasets and set normalisation parameters
     Dataloader = DATALOADER_DICT[config["data"]["type"]]
-    TrainGenerator = Dataloader(config=config["data"], dataset_type="training", dev=dev)
-    ValGenerator = Dataloader(config=config["data"], dataset_type="validation", dev=dev)
+    TrainGenerator = Dataloader(config=config["data"], dataset_type=Subsets.TRAIN, dev=dev)
+    ValGenerator = Dataloader(config=config["data"], dataset_type=Subsets.VALID, dev=dev)
 
     # Create dataloader
     train_ds = tf.data.Dataset.from_generator(
@@ -49,16 +63,22 @@ def get_train_dataloader(config: dict[str, Any], dev: bool) -> DataloaderType:
 
 def get_test_dataloader(
     config: dict[str, Any],
+    subset: str = Subsets.TEST,
     dev: bool = False,
 ) -> tuple[tf.data.Dataset, BaseDataloader]:
 
-    # Inference-specific config settings
-    config["data"]["cv_folds"] = 1
-    config["data"]["fold"] = 0
-
-    # Initialise datasets and set normalisation parameters
     Dataloader = DATALOADER_DICT[config["data"]["type"]]
-    TestGenerator = Dataloader(config=config["data"], dataset_type="validation", dev=dev)
+
+    if subset == Subsets.TEST:
+        # Test-specific config settings
+        config["data"]["cv_folds"] = 1
+        config["data"]["fold"] = 0
+
+    if subset == Subsets.TRAIN:
+        TestGenerator = Dataloader(config=config["data"], dataset_type=Subsets.TRAIN, dev=dev)
+
+    else:
+        TestGenerator = Dataloader(config=config["data"], dataset_type=Subsets.VALID, dev=dev)
 
     # Create dataloader
     output_types = {"source": "float32", "subject_id": tf.string, "target_id": tf.string}
