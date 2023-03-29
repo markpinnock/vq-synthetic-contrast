e8417ab7f0ae8301ced6c99bffa74db828212040
Mark Pinnock <rmappin@ucl.ac.uk>
2023-03-29 14:46:32 +0100

    Add dual stage model

--- src/vq_sce/networks/build_model.py
+++ src/vq_sce/networks/build_model.py
@@ -1,14 +1,16 @@
 import tensorflow as tf
 from typing import Any
+import yaml
 
-from .model import Model, JointModel
+from .model import Model, JointModel, DualModel
 from .multiscale_model import MultiscaleModel, JointMultiscaleModel
 
 MODEL_DICT = {
     "single_scale": Model,
     "multi_scale": MultiscaleModel,
     "single_joint": JointModel,
-    "multi_joint": JointMultiscaleModel
+    "multi_joint": JointMultiscaleModel,
+    "single_dual": DualModel
 }
 
 
@@ -16,17 +18,23 @@ def build_model(config: dict[str, Any], purpose: str = "training") -> tf.keras.M
     scales = config["hyperparameters"]["scales"]
     expt_type = config["expt"]["expt_type"]
     if len(scales) == 1 and expt_type == "single":
-        model_type = "single_scale"
+        model = Model(config)
     elif len(scales) > 1 and expt_type == "single":
-        model_type = "multi_scale"
+        model = MultiscaleModel(config)
     elif len(scales) == 1 and expt_type == "joint":
-        model_type = "single_joint"
+        model = JointModel(config)
     elif len(scales) > 1 and expt_type == "joint":
-        model_type = "multi_joint"
+        model = JointMultiscaleModel(config)
+    elif len(scales) == 1 and expt_type == "dual":
+        with open(config["paths"]["ce_path"] / "config.yml", 'r') as fp:
+            ce_config = yaml.load(fp, yaml.FullLoader)
+        with open(config["paths"]["sr_path"] / "config.yml", 'r') as fp:
+            sr_config = yaml.load(fp, yaml.FullLoader)
+
+        model = DualModel(sr_config, ce_config)
+        assert purpose == "inference"
     else:
         raise ValueError(scales, expt_type)
-
-    model: tf.keras.Model = MODEL_DICT[model_type](config)
 
     if purpose == "training":
         optimiser = tf.keras.optimizers.Adam(*config["hyperparameters"]["opt"], name="opt")
@@ -40,6 +48,11 @@ def build_model(config: dict[str, Any], purpose: str = "training") -> tf.keras.M
         model.build_model()
         model.ce_UNet.load_weights(config["paths"]["expt_path"] / "models" / "ce_model.ckpt")
         model.sr_UNet.load_weights(config["paths"]["expt_path"] / "models" / "sr_model.ckpt")
+        return model
+    elif expt_type == "dual":
+        model.build_model()
+        model.ce_UNet.load_weights(config["paths"]["ce_path"] / "models" / "model.ckpt")
+        model.sr_UNet.load_weights(config["paths"]["sr_path"] / "models" / "model.ckpt")
         return model
     else:
         raise ValueError("Purpose must be 'training' or 'inference'")
