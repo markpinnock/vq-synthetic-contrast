7a534fc9aa56c4c4aba98c6134529017a663b065
Mark Pinnock <rmappin@ucl.ac.uk>
2023-07-27 17:03:35 +0100

    Error with loading model due to class named Model clashing with tf.keras.Model - load checkpoint after instantiating model instead

--- src/vq_sce/networks/build_model.py
+++ src/vq_sce/networks/build_model.py
@@ -2,10 +2,56 @@ from pathlib import Path
 from typing import Any
 
 import tensorflow as tf
+import yaml
 
 from .darts_model import DARTSJointModel, DARTSModel
 from .model import JointModel, Model
 from .multiscale_model import JointMultiscaleModel, MultiscaleModel
+
+# -------------------------------------------------------------------------
+
+
+def get_model(
+    config: dict[str, Any],
+    dev: bool = False,
+    compile_model: bool = True,
+) -> tf.keras.Model:
+    scales = config["hyperparameters"]["scales"]
+    expt_type = config["expt"]["expt_type"]
+    optimisation_type = config["expt"]["optimisation_type"]
+
+    if "darts" in optimisation_type:
+        assert len(scales) == 1, scales
+
+        if expt_type == "single":
+            model = DARTSModel(config)
+        else:
+            model = DARTSJointModel(config)
+
+        if compile_model:
+            model.compile(
+                config["hyperparameters"]["opt"],
+                config["hyperparameters"]["darts_opt"],
+                run_eagerly=dev,
+            )
+
+    else:
+        if len(scales) == 1 and expt_type == "single":
+            model = Model(config)
+        elif len(scales) > 1 and expt_type == "single":
+            model = MultiscaleModel(config)
+        elif len(scales) == 1 and expt_type == "joint":
+            model = JointModel(config)
+        elif len(scales) > 1 and expt_type == "joint":
+            model = JointMultiscaleModel(config)
+        else:
+            raise ValueError(scales, expt_type)
+
+        if compile_model:
+            model.compile(config["hyperparameters"]["opt"], run_eagerly=dev)
+
+    return model
+
 
 # -------------------------------------------------------------------------
 
@@ -20,39 +66,8 @@ def build_model_train(
     :param dev: development mode
     :return: Keras model
     """
-    scales = config["hyperparameters"]["scales"]
-    expt_type = config["expt"]["expt_type"]
-    optimisation_type = config["expt"]["optimisation_type"]
-
     with strategy.scope():
-        if "darts" in optimisation_type:
-            assert len(scales) == 1, scales
-
-            if expt_type == "single":
-                model = DARTSModel(config)
-            else:
-                model = DARTSJointModel(config)
-
-            model.compile(
-                config["hyperparameters"]["opt"],
-                config["hyperparameters"]["darts_opt"],
-                run_eagerly=dev,
-            )
-
-        else:
-            if len(scales) == 1 and expt_type == "single":
-                model = Model(config)
-            elif len(scales) > 1 and expt_type == "single":
-                model = MultiscaleModel(config)
-            elif len(scales) == 1 and expt_type == "joint":
-                model = JointModel(config)
-            elif len(scales) > 1 and expt_type == "joint":
-                model = JointMultiscaleModel(config)
-            else:
-                raise ValueError(scales, expt_type)
-
-            model.compile(config["hyperparameters"]["opt"], run_eagerly=dev)
-
+        model = get_model(config, dev, compile_model=True)
         model.build_model()
 
     return model
@@ -75,6 +90,11 @@ def build_model_inference(
         models = []
 
         for path in expt_path:
+            with open(path / "config.yml") as fp:
+                config = yaml.load(fp, yaml.FullLoader)
+            config["expt"]["local_mb_size"] = None
+            model = get_model(config, compile_model=False)
+
             if epoch is None:
                 ckpts = sorted(
                     list((path / "models").glob("*")),
@@ -87,24 +107,30 @@ def build_model_inference(
                 assert len(ckpts) == 1, ckpts
                 ckpt_path = ckpts[0]
 
+            model.load_weights(ckpt_path / "variables" / "variables")
             models.append(tf.keras.models.load_model(ckpt_path))
 
         model = tf.keras.Sequential(models)
 
     # Otherwise, load single model
     else:
+        with open(expt_path / "config.yml") as fp:
+            config = yaml.load(fp, yaml.FullLoader)
+        config["expt"]["local_mb_size"] = None
+        model = get_model(config, compile_model=False)
+
         if epoch is None:
             ckpts = sorted(
-                list((expt_path / "models").glob("*")),
+                list((expt_path / "models").glob(f"ckpt-{epoch}")),
                 key=lambda x: int(str(x.stem).split("-")[-1]),
             )
             ckpt_path = ckpts[-1]
 
         else:
-            ckpts = list((expt_path / "models" / f"ckpt-{epoch}").glob("*"))
+            ckpts = list((expt_path / "models").glob(f"ckpt-{epoch}"))
             assert len(ckpts) == 1, ckpts
             ckpt_path = ckpts[0]
 
-        model = tf.keras.models.load_model(ckpt_path)
+        model.load_weights(ckpt_path / "variables" / "variables")
 
     return model
