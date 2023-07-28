85a35ba62a667d91468516cfa6dcd836d88d388b
Mark Pinnock <rmappin@ucl.ac.uk>
2023-07-28 16:33:43 +0100

    Update batch script

--- RunMetrics.bat
+++ RunMetrics.bat
@@ -8,21 +8,36 @@ CALL %conda_path%\Scripts\activate.bat base
 CALL %conda_path%\Scripts\activate.bat contrast
 cd %working_dir%
 
-for %%x in (
-    "ce_ss_vq128"
-    ) do (
-        CALL calc_metrics -p %%x -d %data_dir% -s contrast
-        )
+@REM for %%x in (
+@REM     "ce_ss_vq128"
+@REM     ) do (
+@REM         CALL calc_metrics -p %%x -d %data_dir% -s contrast
+@REM         )
 
-for %%x in (
-    "sr_ss_vq512"
-    ) do (
-        CALL calc_metrics -p %%x -d %data_dir% -s super_res
-        )
+@REM for %%x in (
+@REM     "sr_ss_vq512"
+@REM     ) do (
+@REM         CALL calc_metrics -p %%x -d %data_dir% -s super_res
+@REM         )
 
-for %%x in (
-    "jo_ss_vq128"
-    ) do (
-        CALL calc_metrics -p %%x -d %data_dir% -s super_res
-        CALL calc_metrics -p %%x -d %data_dir% -s contrast
-        )
+@REM for %%x in (
+@REM     "jo_ss_vq128"
+@REM     ) do (
+@REM         CALL calc_metrics -p %%x -d %data_dir% -s super_res
+@REM         CALL calc_metrics -p %%x -d %data_dir% -s contrast
+@REM         )
+
+CALL calc_metrics -p "ce_vq8" -d %data_dir% -st "contrast" -ep "2000"
+CALL calc_metrics -p "ce_vq16" -d %data_dir% -st "contrast" -ep "1700"
+CALL calc_metrics -p "ce_vq16" -d %data_dir% -st "contrast" -ep "2000"
+CALL calc_metrics -p "ce_vq32" -d %data_dir% -st "contrast" -ep "1600"
+CALL calc_metrics -p "ce_vq32" -d %data_dir% -st "contrast" -ep "2000"
+CALL calc_metrics -p "ce_vq64" -d %data_dir% -st "contrast" -ep "1600"
+CALL calc_metrics -p "ce_vq64" -d %data_dir% -st "contrast" -ep "2000"
+CALL calc_metrics -p "ce_vq128" -d %data_dir% -st "contrast" -ep "1600"
+CALL calc_metrics -p "ce_vq128" -d %data_dir% -st "contrast" -ep "2000"
+CALL calc_metrics -p "ce_vq256" -d %data_dir% -st "contrast" -ep "1600"
+CALL calc_metrics -p "ce_vq256" -d %data_dir% -st "contrast" -ep "2000"
+CALL calc_metrics -p "ce_vq512" -d %data_dir% -st "contrast" -ep "2000"
+CALL calc_metrics -p "ce_vq1024" -d %data_dir% -st "contrast" -ep "1600"
+CALL calc_metrics -p "ce_vq1024" -d %data_dir% -st "contrast" -ep "2000"
