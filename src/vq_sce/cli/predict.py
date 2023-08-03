203fb1caf1fb553f4200c99da1321fc9540fa9bc
Mark Pinnock <rmappin@ucl.ac.uk>
2023-08-03 11:25:14 +0100

    Update predict to use validation data

--- src/vq_sce/cli/predict.py
+++ src/vq_sce/cli/predict.py
@@ -5,6 +5,7 @@ import yaml
 
 from vq_sce.inference import Inference, MultiScaleInference, SingleScaleInference
 from vq_sce.networks.model import Task
+from vq_sce.utils.dataloaders.build_dataloader import Subsets
 
 
 def main() -> None:
@@ -13,7 +14,8 @@ def main() -> None:
     parser.add_argument("--path", "-p", help="Expt path", type=str)
     parser.add_argument("--data", "-d", help="Data path", type=str)
     parser.add_argument("--original", "-o", help="Original data path", type=str)
-    parser.add_argument("--stage", "-s", help="Joint stage", type=str)
+    parser.add_argument("--stage", "-st", help="Joint stage", type=str)
+    parser.add_argument("--subset", "-su", help="Data subset", type=str)
     parser.add_argument("--epoch", "-ep", help="Model save epoch", type=str)
     parser.add_argument("--minibatch", "-m", help="Minibatch size", type=int, default=8)
     parser.add_argument("--option", "-op", help="`save`, `display`", type=str)
@@ -27,8 +29,12 @@ def main() -> None:
         config = yaml.load(infile, yaml.FullLoader)
 
     config["paths"]["expt_path"] = Path(arguments.path)
-    config["data"]["data_path"] = Path(arguments.data) / "test"
     config["expt"]["mb_size"] = arguments.minibatch
+
+    if arguments.subset == Subsets.TEST:
+        config["data"]["data_path"] = Path(arguments.data) / "test"
+    else:
+        config["data"]["data_path"] = Path(arguments.data) / "train"
 
     # Optional path to original NRRD data (for saving metadata with predictions)
     if arguments.original is None:
