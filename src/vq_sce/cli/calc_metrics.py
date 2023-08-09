4502289377ea5a78135f2a88956f9cef7b931358
Mark Pinnock <rmappin@ucl.ac.uk>
2023-08-09 17:18:58 +0100

    Move calc_intensities.py to calc_metrics.py and delete latter

--- src/vq_sce/cli/calc_metrics.py
+++ src/vq_sce/cli/calc_metrics.py
@@ -1,15 +1,201 @@
 import argparse
-import copy
 from pathlib import Path
+from typing import Any
 
+import numpy as np
 import pandas as pd
-import yaml
+import SimpleITK as itk  # noqa: N813
+from numpy.typing import NDArray
+from skimage.metrics import (
+    mean_squared_error,
+    peak_signal_noise_ratio,
+    structural_similarity,
+)
 
-from vq_sce.inference import Inference, MultiScaleInference, SingleScaleInference
-from vq_sce.networks.model import Task
-from vq_sce.utils.dataloaders.build_dataloader import Subsets, get_test_dataloader
+from vq_sce import HU_MAX, HU_MIN
+from vq_sce.preproc.preprocess import HU_DEFAULT
 
 METRICS = ["L1", "MSE", "pSNR", "SSIM"]
+
+# -------------------------------------------------------------------------
+
+
+def load_and_transform(
+    subject_id: str,
+    paths: dict[str, Path],
+) -> tuple[itk.Image, itk.Image]:
+    ce_candidates = list((paths["contrast"] / subject_id).glob(f"{subject_id}AC*"))
+    assert len(ce_candidates) == 1
+    ce_img = itk.ReadImage(str(ce_candidates[0]))
+
+    pred_candidates = list(paths["predictions"].glob(f"{subject_id}HQ*"))
+    assert len(pred_candidates) == 1
+    pred_img = itk.ReadImage(str(pred_candidates[0]))
+    ce_img = itk.Resample(ce_img, pred_img, defaultPixelValue=HU_DEFAULT)
+
+    hu_filter = itk.ClampImageFilter()
+    hu_filter.SetLowerBound(HU_MIN)
+    hu_filter.SetUpperBound(HU_MAX)
+    pred_img = hu_filter.Execute(pred_img)
+    ce_img = hu_filter.Execute(ce_img)
+
+    transform_path = paths["transforms"] / subject_id
+    transform_candidates = list(
+        transform_path.glob(
+            f"{ce_candidates[0].stem[-3:]}_to_{pred_candidates[0].stem[-3:]}.h5",
+        ),
+    )
+
+    if len(transform_candidates) == 1:
+        transform = itk.ReadTransform(str(transform_candidates[0]))
+        ce_img = itk.Resample(ce_img, transform, defaultPixelValue=HU_DEFAULT)
+
+    return ce_img, pred_img
+
+
+# -------------------------------------------------------------------------
+
+
+def get_bounding_boxes(
+    ce_img: itk.Image,
+    pred_img: itk.Image,
+    bounding_box_data: list[str],
+) -> tuple[dict[str, NDArray[np.int16]], dict[str, NDArray[np.int16]]]:
+    fiduciaries: dict[str, Any] = {"RK": {}, "LK": {}, "VC": {}, "AO": {}}
+    for region in fiduciaries:
+        fiduciaries[region]["L"] = np.zeros(2)
+        fiduciaries[region]["P"] = np.zeros(2)
+
+    for line in bounding_box_data[3:]:
+        fiduciary = line.split(",")
+        region = fiduciary[11][0:2]
+        side = fiduciary[11][-1]
+
+        if side == "R":
+            fiduciaries[region]["L"][0] = -float(fiduciary[1])
+        elif side == "L":
+            fiduciaries[region]["L"][1] = -float(fiduciary[1])
+        elif side == "A":
+            fiduciaries[region]["P"][0] = -float(fiduciary[2])
+        else:
+            fiduciaries[region]["P"][1] = -float(fiduciary[2])
+
+        fiduciaries[region]["S"] = float(fiduciary[3])
+
+    bounds: dict[str, Any] = {"RK": {}, "LK": {}, "VC": {}, "AO": {}}
+
+    for region in fiduciaries.keys():
+        xs = (
+            fiduciaries[region]["L"] - pred_img.GetOrigin()[0]
+        ) / pred_img.GetSpacing()[0]
+        ys = (
+            fiduciaries[region]["P"] - pred_img.GetOrigin()[1]
+        ) / pred_img.GetSpacing()[1]
+
+        try:
+            zs = (
+                fiduciaries[region]["S"] - pred_img.GetOrigin()[2]
+            ) / pred_img.GetSpacing()[2]
+        except KeyError:
+            del bounds[region]
+            continue
+
+        bounds[region]["x"] = slice(*np.round(xs).astype(np.int16))
+        bounds[region]["y"] = slice(*np.round(ys).astype(np.int16))
+        bounds[region]["z"] = np.round(zs).astype(np.int16)
+
+    pred_np = itk.GetArrayFromImage(pred_img)
+    ce_np = itk.GetArrayFromImage(ce_img)
+
+    ce_sub_volumes = dict.fromkeys(bounds.keys())
+    pred_sub_volumes = dict.fromkeys(bounds.keys())
+
+    for region in ce_sub_volumes.keys():
+        roi_bounds = bounds[region]
+        ce_sub_volumes[region] = ce_np[
+            roi_bounds["z"] - 8 : roi_bounds["z"] + 8,
+            roi_bounds["y"],
+            roi_bounds["x"],
+        ]
+        pred_sub_volumes[region] = pred_np[
+            roi_bounds["z"] - 8 : roi_bounds["z"] + 8,
+            roi_bounds["y"],
+            roi_bounds["x"],
+        ]
+
+    return ce_sub_volumes, pred_sub_volumes
+
+
+# -------------------------------------------------------------------------
+
+
+def calc_global_metrics(
+    ce_volume: itk.Image,
+    pred_volume: itk.Image,
+) -> dict[str, float]:
+    """Calculate L1, MSE, pSNR, SSIM between predicted and ground truth image."""
+    ce_volume_np = itk.GetArrayFromImage(ce_volume)
+    pred_volume_np = itk.GetArrayFromImage(pred_volume)
+
+    metrics = {
+        "MSE": float(mean_squared_error(ce_volume_np, pred_volume_np)),
+        "pSNR": float(
+            peak_signal_noise_ratio(
+                ce_volume_np,
+                pred_volume_np,
+                data_range=HU_MAX - HU_MIN,
+            ),
+        ),
+        "SSIM": float(
+            structural_similarity(
+                ce_volume_np,
+                pred_volume_np,
+                data_range=HU_MAX - HU_MIN,
+            ),
+        ),
+    }
+    metrics["L1"] = float(np.mean(np.abs(ce_volume_np - pred_volume_np)))
+
+    return metrics
+
+
+# -------------------------------------------------------------------------
+
+
+def calc_intensity_diffs(
+    ce_sub_volume: dict[str, NDArray[np.int16]],
+    pred_sub_volume: dict[str, NDArray[np.int16]],
+) -> dict[str, float]:
+    results = {}
+
+    for region in ce_sub_volume.keys():
+        results[region] = np.mean(
+            np.abs(ce_sub_volume[region] - pred_sub_volume[region]),
+        )
+
+    return results
+
+
+# -------------------------------------------------------------------------
+
+
+def calc_intensities(pred_sub_volume: dict[str, NDArray[np.int16]]) -> dict[str, float]:
+    results = {}
+
+    for region in pred_sub_volume.keys():
+        results[region] = float(np.mean(pred_sub_volume[region]))
+
+    return results
+
+
+# -------------------------------------------------------------------------
+
+
+def calc_glcm(pred_sub_volume: dict[str, NDArray[np.int16]]) -> dict[str, float]:
+    pass
+
+
+# -------------------------------------------------------------------------
 
 
 def main() -> None:
@@ -17,107 +203,153 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", "-p", help="Expt path", type=str)
     parser.add_argument("--data", "-d", help="Data path", type=str)
-    parser.add_argument("--stage", "-st", help="Joint stage", type=str)
     parser.add_argument("--subset", "-su", help="Data subset", type=str)
-    parser.add_argument("--epoch", "-ep", help="Model save epoch", type=str)
-    parser.add_argument("--minibatch", "-m", help="Minibatch size", type=int, default=8)
-    parser.add_argument("--dev", "-dv", help="Development mode", action="store_true")
     arguments = parser.parse_args()
 
-    expt_path = Path(arguments.path)
+    paths = {}
+    paths["contrast"] = Path(arguments.data) / "Images"
+    paths["predictions"] = Path(arguments.path)
+    paths["transforms"] = Path(arguments.data) / "Transforms"
+    paths["bounding_boxes"] = Path(arguments.data) / "BoundingBoxes"
 
-    # Parse config json
-    with open(expt_path / "config.yml") as infile:
-        config = yaml.load(infile, yaml.FullLoader)
+    model_name = paths["predictions"].parent.stem
+    epochs = paths["predictions"].stem.split("-")[1]
 
-    config["paths"]["expt_path"] = Path(arguments.path)
-    config["paths"]["original_path"] = None
-    config["data"]["data_path"] = Path(arguments.data) / "test"
-    config["expt"]["mb_size"] = arguments.minibatch
+    subject_ids = []
 
-    # Development mode if necessary
-    if arguments.dev:
-        dims = config["data"]["source_dims"]
-        config["data"]["source_dims"] = [dims[0], dims[1] // 4, dims[2] // 4]
-        dims = config["data"]["target_dims"]
-        config["data"]["target_dims"] = [dims[0], dims[1] // 4, dims[2] // 4]
-        config["data"]["down_sample"] = 4
-    else:
-        config["data"]["down_sample"] = 1
+    for img in paths["predictions"].glob("*"):
+        if img.stem[0:6] not in subject_ids:
+            subject_ids.append(img.stem[0:6])
 
-    config_copy = copy.deepcopy(
-        config,
-    )  # Avoid subset-specific params being overwritten
-    inference: Inference
+    global_metrics: dict[str, list[str | float]] = {
+        "id": [],
+        "L1": [],
+        "MSE": [],
+        "pSNR": [],
+        "SSIM": [],
+    }
 
-    if len(config["hyperparameters"]["scales"]) == 1:
-        inference = SingleScaleInference(
-            config_copy,
-            stage=arguments.stage,
-            epoch=arguments.epoch,
-        )
-    else:
-        inference = MultiScaleInference(
-            config_copy,
-            stage=arguments.stage,
-            epoch=arguments.epoch,
-        )
+    intensity_diffs: dict[str, list[str | float]] = {
+        "id": [],
+        "RK": [],
+        "LK": [],
+        "VC": [],
+        "AO": [],
+    }
+    intensities: dict[str, list[str | float]] = {
+        "id": [],
+        "RK": [],
+        "LK": [],
+        "VC": [],
+        "AO": [],
+    }
+    regions = ["RK", "LK", "VC", "AO"]
 
-    if arguments.subset is None:
-        subsets = Subsets
-    else:
-        assert arguments.subset in list(Subsets), (arguments.subset, list(Subsets))
-        subsets = [arguments.subset]  # type: ignore
+    for subject_id in subject_ids:
+        with open(paths["bounding_boxes"] / f"{subject_id}.fcsv") as fp:
+            bounding_box = fp.readlines()
 
-    for subset in subsets:
-        config_copy = copy.deepcopy(
-            config,
-        )  # Avoid subset-specific params being overwritten
+        ce_img, pred_img = load_and_transform(subject_id, paths)
+        ce_sub, pred_sub = get_bounding_boxes(ce_img, pred_img, bounding_box)
 
-        if subset == Subsets.TEST:
-            config_copy["data"]["data_path"] = Path(arguments.data) / "test"
-        else:
-            config_copy["data"]["data_path"] = Path(arguments.data) / "train"
-
-        if config_copy["expt"]["expt_type"] == Task.JOINT:
-            config_copy["data"]["type"] = arguments.stage
-        else:
-            assert arguments.stage == config_copy["data"]["type"]
-
-        # Set up dataset and override Inference class's default one
-        test_ds, test_generator = get_test_dataloader(config_copy, subset=subset)
-        inference.test_ds = test_ds
-        inference.TestGenerator = test_generator
-        inference.data_path = config_copy["data"]["data_path"]
-
-        metric_dict = inference.run(option="metrics")
+        # Calculate global metrics
+        subject_global_metrics = calc_global_metrics(ce_img, pred_img)
+        global_metrics["id"].append(subject_id)
 
         for metric in METRICS:
-            csv_name = f"{arguments.stage}_{subset}_{metric}"
+            global_metrics[metric].append(subject_global_metrics[metric])
 
-            # Create dataframe if not present
-            df_path = config_copy["paths"]["expt_path"].parent / f"{csv_name}.csv"
+        # Calculate bounding box L1
+        subject_intensity_diffs = calc_intensity_diffs(ce_sub, pred_sub)
+        intensity_diffs["id"].append(subject_id)
 
+        for region in regions:
             try:
-                df = pd.read_csv(df_path, index_col=0)
+                intensity_diffs[region].append(subject_intensity_diffs[region])
 
-            except FileNotFoundError:
-                df = pd.DataFrame(index=metric_dict["id"])
-                df[
-                    f"{config_copy['paths']['expt_path'].stem}_{arguments.epoch}"
-                ] = metric_dict[metric]
-            else:
-                new_df = pd.DataFrame(
-                    metric_dict[metric],
-                    index=metric_dict["id"],
-                    columns=[
-                        f"{config_copy['paths']['expt_path'].stem}_{arguments.epoch}",
-                    ],
-                )
-                df = df.join(new_df, how="outer")
+            except KeyError:
+                intensity_diffs[region].append(None)
 
-            df.to_csv(df_path, index=True)
+        # Calculate bounding box intensities
+        subject_intensities = calc_intensities(pred_sub)
+        intensities["id"].append(subject_id)
 
+        for region in regions:
+            try:
+                intensities[region].append(subject_intensities[region])
 
-if __name__ == "__main__":
-    main()
+            except KeyError:
+                intensities[region].append(None)
+
+    # Save global metrics
+    csv_name = f"contrast_{arguments.subset}_global"
+
+    # Create dataframe if not present
+    df_path = paths["predictions"].parents[1] / f"{csv_name}.csv"
+
+    try:
+        df = pd.read_csv(df_path, index_col=0, header=[0, 1])
+
+        # except FileNotFoundError:
+        #     df = pd.DataFrame(index=metric_dict["id"])
+        #     df[
+        #         f"{config_copy['paths']['expt_path'].stem}_{arguments.epoch}"
+        #     ] = metric_dict[metric]
+        # else:
+        #     new_df = pd.DataFrame(
+        #         metric_dict[metric],
+        #         index=metric_dict["id"],
+        #         columns=[
+        #             f"{config_copy['paths']['expt_path'].stem}_{arguments.epoch}",
+        #         ],
+        #     )
+        #     df = df.join(new_df, how="outer")
+
+    except FileNotFoundError:
+        df = pd.DataFrame(
+            index=global_metrics["id"],
+            columns=pd.MultiIndex.from_product([METRICS, [f"{model_name}-{epochs}"]]),
+        )
+
+    finally:
+        for metric in METRICS:
+            df[(metric, f"{model_name}-{epochs}")] = global_metrics[metric]
+
+        df.to_csv(df_path, index=True)
+
+    # Save bounding box L1
+    csv_name = f"contrast_{arguments.subset}_focal_L1"
+    df_path = paths["predictions"].parents[1] / f"{csv_name}.csv"
+
+    try:
+        df = pd.read_csv(df_path, index_col=0, header=[0, 1])
+
+    except FileNotFoundError:
+        df = pd.DataFrame(
+            index=intensity_diffs["id"],
+            columns=pd.MultiIndex.from_product([regions, [f"{model_name}-{epochs}"]]),
+        )
+
+    finally:
+        for region in regions:
+            df[(region, f"{model_name}-{epochs}")] = intensity_diffs[region]
+
+        df.to_csv(df_path, index=True)
+
+    # Save bounding box intensities
+    csv_name = f"contrast_{arguments.subset}_focal_HU"
+    df_path = paths["predictions"].parents[1] / f"{csv_name}.csv"
+
+    try:
+        df = pd.read_csv(df_path, index_col=0, header=[0, 1])
+
+    except FileNotFoundError:
+        df = pd.DataFrame(
+            index=intensities["id"],
+            columns=pd.MultiIndex.from_product([regions, [f"{model_name}-{epochs}"]]),
+        )
+    finally:
+        for region in regions:
+            df[(region, f"{model_name}-{epochs}")] = intensities[region]
+
+        df.to_csv(df_path, index=True)
